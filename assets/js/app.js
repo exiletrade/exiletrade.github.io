1a503f360a0e4d0f70a43027f851b12a0acf594d
thirdy <thirdy.derivera@gmail.com>
2016-03-22 04:50:03 +0800

    improved online label

--- assets/js/app.js
+++ assets/js/app.js
@@ -18428,7 +18428,7 @@ function parseSearchInput(_terms, input) {
 	var lsts = input.match(regex);
 	var _input = input.replace(regex, 'LST');
 	var parseResult = parseSearchInputTokens(_input);
-	debugOutput(parseResult, 'info');
+	
 	var i = 0;
 	parseResult.queryString = parseResult.queryString.replace('LST', function (match) {
 		var lst = lsts[i];
@@ -18933,26 +18933,22 @@ function buildElasticJSONRequestBody(searchQuery, _size, sortKey, sortOrder, onl
 				$scope.showSpinner = false;
 				return;
 			}
-
-			// TODO, this is a bit messy here
 			
 			//var onlineplayersLadderPromise = playerOnlineService.getLadderOnlinePlayers($scope.options.leagueSelect.value);
-			var onlineplayersStashPromise = $scope.switchOnlinePlayersOnly ? playerOnlineService.getStashOnlinePlayers(es) : $q.when([]);
+			var onlineplayersStashPromise = playerOnlineService.getStashOnlinePlayers(es);
 
 			$q.all({
 			  //onlineplayersLadder: onlineplayersLadderPromise,
 			  onlineplayersStash: onlineplayersStashPromise
 			}).then(function(results) {
-				$scope.onlinePlayers = [];
-
-				if ($scope.switchOnlinePlayersOnly) {
-					//var onlineplayersLadder = results.onlineplayersLadder.data;
-					var onlineplayersStash  = results.onlineplayersStash.aggregations.filtered.sellers.buckets;
-					playerOnlineService.cacheStashOnlinePlayers(results.onlineplayersStash)
-					$scope.onlinePlayers = buildListOfOnlinePlayers([], onlineplayersStash);
-				}
 				
-			   	var esBody = buildElasticJSONRequestBody(searchQuery, limit, sortKey, sortOrder, $scope.onlinePlayers);
+				//var onlineplayersLadder = results.onlineplayersLadder.data;
+				var onlineplayersStash  = results.onlineplayersStash.aggregations.filtered.sellers.buckets;
+				playerOnlineService.cacheStashOnlinePlayers(results.onlineplayersStash)
+				$scope.onlinePlayers = buildListOfOnlinePlayers([], onlineplayersStash);
+				
+				var accountNamesFilter = $scope.switchOnlinePlayersOnly ? $scope.onlinePlayers : [];
+			   	var esBody = buildElasticJSONRequestBody(searchQuery, limit, sortKey, sortOrder, accountNamesFilter);
 			   	$scope.elasticJsonRequest = angular.toJson(esBody, true);
 			   	return es.search({
 					index: 'index',
@@ -18990,6 +18986,7 @@ function buildElasticJSONRequestBody(searchQuery, _size, sortKey, sortOrder, onl
 				item.shop['addedHoursAgo'] = (Math.abs(now.getTime() - added.getTime()) / hourInMillis).toFixed(2);
 				item.shop['modifiedHoursAgo'] = (Math.abs(now.getTime() - modified.getTime()) / hourInMillis).toFixed(2);
 				item.shop['updatedHoursAgo'] = (Math.abs(now.getTime() - updated.getTime()) / hourInMillis).toFixed(2);
+				item.isOnline = $scope.onlinePlayers.indexOf(item.shop['sellerAccount']) != -1;
 			}
 		}
 
